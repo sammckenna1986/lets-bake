996b6c85ebbc7b93d724fb38d3a4ce9786208127
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-20 20:15:35 +0100

    setting up heroku deployment

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT","5000")), debug=False)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
 
 
 @app.route('/')
