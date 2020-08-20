0787ea6787e7846f9e910d699003dd85e5d8ccef
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-20 20:05:52 +0100

    setting up heroku deployment

--- run.py
+++ run.py
@@ -4,11 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")),
-        debug=False),
-
-
 @app.route('/')
 def hello():
     return 'Hello World ...again'
