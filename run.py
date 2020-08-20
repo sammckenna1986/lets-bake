c64beb44f3117e30928124720bda1093f57b199f
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-20 20:01:13 +0100

    setting up heroku deployment

--- run.py
+++ run.py
@@ -3,22 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config.update(
+app.secret_key = os.getenv("SECRET", "randomstring123")
 
-    #Set the secret key to a sufficiently random value
-    SECRET_KEY=os.urandom(24),
 
-    #Set the session cookie to be secure
-    SESSION_COOKIE_SECURE=True,
-
-    #Set the session cookie for our app to a unique name
-    SESSION_COOKIE_NAME='YourAppName-WebSession',
-
-    #Set CSRF tokens to be valid for the duration of the session. This assumes you’re using WTF-CSRF protection
-    WTF_CSRF_TIME_LIMIT=None
-
-)
-
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")),
+        debug=False),
 
 
 @app.route('/')
