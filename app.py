ebf5c527b8623e6be2f02e1287b8743d1ea51e89
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-24 18:39:30 +0100

    setting up heroku deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
+
 
 
 @app.route('/')
@@ -11,4 +11,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
