d5fe7a444aaed6ac8cb2b277a420a60b05ec00ba
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-27 14:48:43 +0100

    connecting to mongodb take 2

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo, pymongo
 
 
-
-
 app = Flask(__name__)
-
-
-MONGO_URI = os.environ.get(“MONGO_URI”)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
 
