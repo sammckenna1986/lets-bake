16e0b2666f4a6d383967c3f3abee36915f6c9db2
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-08-29 21:15:19 +0100

    app.py renamed to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
@@ -8,6 +9,7 @@ mongo = PyMongo(app)
 cluster = MongoClient("mongodb://claireroberts1403:m0ng0DB2020@letsbake-shard-00-00.mizcn.mongodb.net:27017,letsbake-shard-00-01.mizcn.mongodb.net:27017,letsbake-shard-00-02.mizcn.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-e54zvz-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = cluster["Recipe_Book"]
 collection = db["Recipes"]
+
 
 
 results = collection.find({"name":"brownie"})
@@ -33,17 +35,16 @@ def page_not_found(error):
 def upload_recipe():
    return render_template("upload_recipe.html")
 
-@app.route('/Search_Results')
-def recipe_search_display():
-   return render_template("recipe_search_display.html")
-
 
 #app.route('/Categories{% categorie_name %}')
 # def categories():
 #   return render_template("categories.html")
 
-#def recipe_display:
-#    db.Recipe_Book.find({}, {"name": 1});
+
+@app.route('/Search_Results')
+def recipe_display():
+    return render_template("recipes_search_display.html", recipes=mongo.db.recipes.find())
+
 
 
 if __name__ == '__main__':
