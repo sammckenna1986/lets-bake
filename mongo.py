8e2ceec06a3123651b2fe6efca97e64afe59c920
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-09-02 18:28:00 +0100

    tests complete to mongodb to add find delete to the database

--- mongo.py
+++ mongo.py
@@ -1,21 +1,13 @@
 import pymongo
 
-
-cluster = pymongo.MongoClient("mongodb+srv://claireroberts1403:M0ng0DB2020@letsbake.mizcn.mongodb.net/Recipe_Book?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient(
+    "mongodb+srv://claireroberts1403:M0ng0DB2020@letsbake.mizcn.mongodb.net/Recipe_Book?retryWrites=true&w=majority")
 db = cluster["Recipe_Book"]
 collection = db["Recipes"]
 
 
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected!")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
+results = collection.find({})
 
 
-
-post = {"name": "Brilliant Birthday Cake"}
-
-collection.insert_one(post)
+for x in results:
+    print(x)
